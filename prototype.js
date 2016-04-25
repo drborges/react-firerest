2d91cbbab0770d46bd6d2b8bb9f8e14a20171f70
Diego Borges <drborges.cic@gmail.com>
2016-04-25 14:23:12 -0300

    Update prototype with some more ideas

--- prototype.js
+++ prototype.js
@@ -4,60 +4,36 @@
       /users/:user/lessons/:lesson/tests/:test
 */
 
-const FirebaseResource = {
-  get ref() { return new Firebase(`https://${app}.firebaseio.com${path}`) },
-  get app() { return 'userxp' },
-  get path() { return '/users/:user' },
-  get key() { return 'user' } // path.split('/').last.slice(1)
-}
-
-class FirebaseResourceCreator {
-  constructor(resource) {
-    this.resource = resource
-    this.data = {}
-  }
-
-  merge(state) {
-    state[this.resource.key] = this.data
-  }
-
-  post() {
-    this.resource.ref.push(this.data)
-  }
-}
-
-@firebase('userxp')
-@firebase.POST('/users')
+@firerest.POST({ app: 'userxp', path: '/users' })
 class NewUser extends React.Component {
   render() {
     return (
         <div>
           <input type="text"
-                 valueLink={this.state.data.linkTo('name')} />
+                 valueLink={this.state.user.linkTo('name')} />
           <input type="text"
-                 valueLink={this.state.data.linkTo('level')} />
-          <button onClick={this.state.date.post}>New</button>
+                 valueLink={this.state.user.linkTo('level')} />
+
+          <button onClick={this.resource.post(this.state.user))}>New</button>
         </div>
       )
   }
 }
 
-@firebase('userxp')
-@firebase.GET('/users/:user')
+@firerest.GET({ app: 'userxp', path: '/users/:teacher' })
 class ShowUser extends React.Component {
   render() {
     return (
         <div>
-          <span>Name: {this.state.user.name}</span>
-          <span>Level: {this.state.user.level}</span>
-          <button onClick={this.state.user.get}>Force Refresh</button>
+          <span>Name: {this.state.teacher.name}</span>
+          <span>Level: {this.state.teacher.level}</span>
+          <button onClick={this.resource.get}>Force Refresh</button>
         </div>
       )
   }
 }
 
-@firebase('userxp')
-@firebase.PUT('/users/:user')
+@firerest.PUT({ app: 'userxp', path: '/users/:user'})
 class EditUser extends React.Component {
   render() {
     return (
@@ -68,24 +44,23 @@ class EditUser extends React.Component {
           <input type="text"
                  value={this.state.user.level}
                  onChange={this.state.user.update('level')} />
-          <button onClick={this.state.user.put}>Save</button>
+
+               <button onClick={this.resource.put(this.state.user))}>Save</button>
         </div>
       )
   }
 }
 
-@firebase('userxp')
-@firebase.DELETE('/users/:user')
+@firerest.DELETE({ app: 'userxp', path: '/users/:user'})
 class DeleteUser extends React.Component {
 render() {
     return (
-        <button onClick={this.state.user.delete}>Delete</button>
+        <button onClick={this.resource.delete}>Delete</button>
       )
   }
 }
 
-@firebase('userxp')
-@firebase.GET('/users')
+@firerest.GET({ app: 'userxp', path: '/users'})
 class ListUser extends React.Component {
   render() {
     let rows = Object.keys(this.state.users).map(userId => {
@@ -97,6 +72,7 @@ class ListUser extends React.Component {
   }
 }
 
+// Stateless component
 const UserRow = props =>
   <li>
     <span>Name: {props.user.name} ({props.user.level})</span>
@@ -104,58 +80,48 @@ const UserRow = props =>
     <DeleteUser user={props.user} />
   </li>
 
-@firebase('userxp')
-@firebase.POST('/users/:user/lessons')
+@firerest.POST({ app: 'userxp', path: '/users/:teacher/lessons'})
 class NewLesson extends React.Component {
+
+  @firerest.On('changed', 'teacher')
+  OnTeacherChanged(event) {
+    this.state.teacher = event.value
+    this.setState(this.state)
+  }
+
   render() {
     return (
         <div>
           <h1>New Lesson Teached by User:</h1>
-          <ShowUser user={this.props.user} />
+          <ShowUser user={this.state.teacher} />
 
           <input type="text"
-                 value={this.state.data.title}
-                 onChange={this.state.data.update('title')} />
+                 valueLink={this.state.lesson.linkTo('title')} />
           <input type="text"
-                 value={this.state.data.duration}
-                 onChange={this.state.data.update('duration')} />
-          <button onClick={this.state.date.post}>New</button>
+                 valueLink={this.state.lesson.linkTo('duration')} />
+
+          <button onClick={this.resource.post(this.state.lesson)}>New</button>
         </div>
       )
   }
 }
 
-@firebase('userxp')
-@firebase.GET('/users/:user/lessons/:lesson')
+// This decorator causes all resources that compose the 'path' to be automatically fetched
+// and stored as properties (in this case 'teacher' and 'lesson') in the component's state object
+@firerest.EagerFetchResources
+@firerest.GET({ app: 'userxp', path: '/users/:teacher/lessons/:lesson' })
 class ShowLesson extends React.Component {
   render() {
     return (
         <div>
           <span>Title: {this.state.lesson.title}</span>
           <span>Duration: {this.state.lesson.duration}</span>
-          <span>Teached By: {this.state.user.name}</span>
-          <button onClick={this.state.lesson.get}>Force Refresh</button>
+          <span>Teached By: {this.state.teacher.name}</span>
         </div>
       )
   }
 }
 
-<ShowLesson user={this.state.user} lesson={this.state.lesson} />
+Usage:
 
-@firebase('userxp')
-@firebase.PUT('/users/:user/lessons/:lesson')
-class EditLesson extends React.Component {
-  render() {
-    return (
-        <div>
-          <input type="text"
-                 value={this.state.lesson.title}
-                 onChange={this.state.lesson.update('title')} />
-          <input type="text"
-                 value={this.state.lesson.duration}
-                 onChange={this.state.lesson.update('duration')} />
-          <button onClick={this.state.lesson.put}>Save</button>
-        </div>
-      )
-  }
-}
+<ShowLesson teacher={this.state.userId} lesson={this.state.lessonId} />
